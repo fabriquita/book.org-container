7f2a2ea43767fdd3f746b03dae6050391e3f8d5f
Arnold Paye <arnoldpayet@gmail.com>
2015-07-11 11:08:57 -0400

    add index and detail pages

---
+++ detail.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Detail Page</title>
+    <link rel="stylesheet" type="text/css" href="./assets/stylesheets/main.css">
+</head>
+<body>
+  
+  <h1>A Game of Thrones</h1>
+  <div class="center">
+  <img src="./assets/images/a_game_of_thrones.jpg"/>
+  </div>
+  <div><h2>This is with style</h2><p>This is a paragraph</p><ul><li>First</li><li>Second</li></ul></div>
+            
+  <a href="index.html">Atras</a>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Book.org Container</title>
+        <link rel="stylesheet" type="text/css" href="./assets/stylesheets/main.css">
+    </head>
+    <body>
+        <h1>Book.org Container</h1>
+        <ul class="wrapper columns-3">
+          <li>
+            <a href="detail.php?id=1">
+              <img src="./assets/images/a_game_of_thrones.jpg"/>
+            </a>
+            <h3>A Game of Thrones</h3>
+            <p>First book  of the serie.</p>
+          </li>
+
+          <li>
+            <a href="detail.php?id=2">
+              <img src="./assets/images/being_nixon.jpg"/>
+            </a>
+            <h3>Being Nixon</h3>
+            <p>The New York Times bestselling author of Ike</p>
+          </li>
+
+          <li>
+            <a href="detail.php?id=3">
+              <img src="./assets/images/pirate_hunters.jpg"/>
+            </a>
+            <h3>Pirate Hunters</h3>
+            <p>A thrilling new adventure of danger and deep-</p>
+          </li>
+
+          <li>
+            <a href="detail.php?id=4">
+              <img src="./assets/images/truth_or_die.jpg"/>
+            </a>
+            <h3>Truth or Die</h3>
+            <p>New York attorney Trevor Mann's world shatter</p>
+          </li>
+
+          <li>
+            <a href="detail.php?id=5">
+              <img src="./assets/images/the_song_of_achilles.jpg"/>
+            </a>
+            <h3>The song of Achilles</h3>
+            <p>A tale of gods, kings, immortal fame...</p>
+          </li>
+        </ul>
+    </body>
+</html>
