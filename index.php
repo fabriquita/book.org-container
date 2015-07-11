f750865e6058bcbabca0bff9e001f07af10772d3
Arnold Paye <arnoldpayet@gmail.com>
2015-07-11 11:25:36 -0400

    index page

--- index.php
+++ index.php
@@ -6,46 +6,35 @@
     </head>
     <body>
         <h1>Book.org Container</h1>
-        <ul class="wrapper columns-3">
-          <li>
-            <a href="detail.php?id=1">
-              <img src="./assets/images/a_game_of_thrones.jpg"/>
-            </a>
-            <h3>A Game of Thrones</h3>
-            <p>First book  of the serie.</p>
-          </li>
+        <?php
+            require 'vendor/autoload.php';
+            use GuzzleHttp\Client;
+            use GuzzleHttp\Message\Response;
 
-          <li>
-            <a href="detail.php?id=2">
-              <img src="./assets/images/being_nixon.jpg"/>
-            </a>
-            <h3>Being Nixon</h3>
-            <p>The New York Times bestselling author of Ike</p>
-          </li>
+            $client = new Client();
 
-          <li>
-            <a href="detail.php?id=3">
-              <img src="./assets/images/pirate_hunters.jpg"/>
-            </a>
-            <h3>Pirate Hunters</h3>
-            <p>A thrilling new adventure of danger and deep-</p>
-          </li>
-
-          <li>
-            <a href="detail.php?id=4">
-              <img src="./assets/images/truth_or_die.jpg"/>
-            </a>
-            <h3>Truth or Die</h3>
-            <p>New York attorney Trevor Mann's world shatter</p>
-          </li>
-
-          <li>
-            <a href="detail.php?id=5">
-              <img src="./assets/images/the_song_of_achilles.jpg"/>
-            </a>
-            <h3>The song of Achilles</h3>
-            <p>A tale of gods, kings, immortal fame...</p>
-          </li>
-        </ul>
+            $base_url = "http://localhost:8080/nucleus-backend/book/";
+            try {
+                $response = $client->get($base_url);
+                if ($response->getStatusCode() == 200) {
+                    $body = json_decode($response->getBody());
+                    echo '<ul class="wrapper columns-3">';
+                    // iterate $body array
+                    foreach($body->content as $book) {
+                        echo '<li>';
+                        echo '<a href="detail.php">';
+                        echo '<h3>' . $book->title .'</h3>';
+                        echo '</a>';
+                        echo '<p>' . $book->author . '</p>';
+                        echo '</li>';
+                    }
+                    echo '</ul>';
+                } else {
+                    echo 'What if? ' . $response->getStatus();
+                }
+            } catch (Exception $e) {
+                echo $e->getMessage();
+            }
+        ?>
     </body>
 </html>
